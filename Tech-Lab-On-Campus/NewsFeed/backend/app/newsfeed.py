b07c77b1f3c6a88c9f3b801ec0929dd50c3e840e
vrindasehgal <81605395+vrindasehgal@users.noreply.github.com>
2025-04-01 19:35:07 -0400

    Update newsfeed.py
    backend changes

--- Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
+++ Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 from datetime import datetime
+from app.utils.redis import REDIS_CLIENT
 
 
 @dataclass
@@ -21,11 +22,27 @@ def get_all_news() -> list[Article]:
     # 1. Use Redis client to fetch all articles
     # 2. Format the data into articles
     # 3. Return a list of the articles formatted 
-    return []
+    articles = REDIS_CLIENT.get_entry("all_articles")
+    return [format_article(article) for article in articles]
 
 
 def get_featured_news() -> Article | None:
     """Get the featured news article from the datastore."""
     # 1. Get all the articles
     # 2. Return as a list of articles sorted by most recent date
-    return None
+    articles = get_all_news
+    return articles.sort(reverse=True, key= (lambda article: article.publish_date))
+
+def format_article(data: dict) -> Article:
+    """Get all news articles from the datastore."""
+    # 1. Use Redis client to fetch all articles
+    # 2. Format the data into articles
+    # 3. Return a list of the articles formatted 
+    return Article (
+        author = data["author"],
+        title = data["title"],
+        body = data["text"],
+        publish_date = datetime.fromisoformat(data["published"]),
+        image_url = data["main_image"],
+        url = data["URL"]    
+    )
